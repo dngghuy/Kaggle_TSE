0f3fb63a532e04b03db02b12fe6eb146f59c0077
Gia-Huy Dang <huydg@mti-tech.vn>
2020-04-07 16:28:20 +0700

    adding sentiment

--- src/engine.py
+++ src/engine.py
@@ -6,11 +6,12 @@ from tqdm import tqdm
 import numpy as np
 
 
-def loss_func(o1, o2, t1, t2):
+def loss_func(o1, o2, o3, t1, t2, t3):
     l1 = nn.BCEWithLogitsLoss()(o1, t1)
     l2 = nn.BCEWithLogitsLoss()(o2, t2)
+    l3 = nn.CrossEntropyLoss()(o3, t3)
 
-    return l1 + l2
+    return l1 + l2 + l3
 
 
 # TODO: Using Pytorch lightning
@@ -24,6 +25,7 @@ def train_fn(data_loader, model, optimizer, device, scheduler):
         mask = d['mask']
         targets_start = d['targets_start']
         targets_end = d['targets_end']
+        sentiment = d['sentiment']
 
         ids = ids.to(device, dtype=torch.long)
         token_type_ids = token_type_ids.to(device, dtype=torch.long)
@@ -32,13 +34,13 @@ def train_fn(data_loader, model, optimizer, device, scheduler):
         targets_end = targets_end.to(device, dtype=torch.float)
 
         optimizer.zero_grad()
-        outputs1, outputs2 = model(
+        outputs1, outputs2, outputs3 = model(
             ids=ids,
             mask=mask,
             token_type_ids=token_type_ids
         )
 
-        loss = loss_func(outputs1, outputs2, targets_start, targets_end)
+        loss = loss_func(outputs1, outputs2, targets_start, targets_end, sentiment)
         loss.backward()
         optimizer.step()
         scheduler.step()
@@ -67,12 +69,11 @@ def eval_fn(data_loader, model, device):
             orig_selected = d["original_selected"]
             orig_tweet = d["original_tweet"]
 
-
             ids = ids.to(device, dtype=torch.long)
             token_type_ids = token_type_ids.to(device, dtype=torch.long)
             mask = mask.to(device, dtype=torch.long)
 
-            outputs1, outputs2 = model(
+            outputs1, outputs2, sent = model(
                 ids=ids,
                 mask=mask,
                 token_type_ids=token_type_ids
--- src/model.py
+++ src/model.py
@@ -10,6 +10,8 @@ class BERTBaseUncased(nn.Module):
         self.bert_drop = nn.Dropout(0.3)
         # Header ~ customize later
         self.l0 = nn.Linear(768, 2)
+        self.l1 = nn.Linear(768, 3)
+
 
     def forward(self, ids, mask, token_type_ids):
         # Currently not using sentiment
@@ -19,9 +21,10 @@ class BERTBaseUncased(nn.Module):
             token_type_ids=token_type_ids
         )
         logits = self.l0(sequence_output)
+        sentiment_pred = self.l1(sequence_output)
         # (batch size, num tokens, 2)
         start_logits, end_logits = logits.split(1, dim=-1)
         start_logits = start_logits.squeeze(-1)
         end_logits = end_logits.squeeze(-1)
 
-        return start_logits, end_logits
+        return start_logits, end_logits, sentiment_pred
